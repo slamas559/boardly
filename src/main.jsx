a7c64c3c30673ce39359a56d3e425f2283adadf4
Salam Abdulsalam <abdulsalamabayomi300@gmail.com>
2025-09-13 03:22:04 +0100

    Socket Provider solved

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { SocketProvider } from './context/socketContext.jsx'
+// import { SocketProvider } from './context/socketContext.jsx'
+import { SocketProvider } from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
